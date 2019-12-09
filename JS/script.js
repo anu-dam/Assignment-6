a56e9b57a81f521fa1b71b19ab2db452aa36c1b7
Anuradha Dambaladeniya <anuradhadambaladeniya@Anuradhas-MacBook-Pro.local>
2019-12-09 19:42:02 +1030

    forcast update

--- JS/script.js
+++ JS/script.js
@@ -1,7 +1,7 @@
 
 var city;
 
-var today = moment().format('LL'); 
+var today = moment().format('LL');
 
 function callApi(city) {
 
@@ -12,13 +12,13 @@ function callApi(city) {
         method: "GET"
     }).then(function (response) {
 
-        
+
 
         $("#city").text("Current Weather in " + response.name);
         $("#date").text(today);
-        
-        
-        $("#temp").text("Temprature : " + response.main.temp + "C");
+
+
+        $("#temp").text("Temprature : " + response.main.temp + " C");
         $("#humid").text("Humidity : " + response.main.humidity);
         $("#wind").text("Wind Speed : " + response.wind.speed);
         $("#pr").text("Pressure : " + response.main.pressure);
@@ -30,7 +30,7 @@ function callApi(city) {
 
         $('#icon').empty();
         $('#icon').append(img);
-        
+
     });
 }
 
@@ -38,109 +38,109 @@ $("#searchBtn").on("click", function () {
 
     var city = $("#select-city").val();
 
-    callApi(city); 
+    callApi(city);
     previous(city);
 
 });
 
-$(".selection").on("click", function(){
+$(".selection").on("click", function () {
 
-var city = event.target.id;
-callApi(city);
-previous(city);
+    var city = event.target.id;
+    callApi(city);
+    previous(city);
 
 
 
 });
 
-function previous(city){ 
+function previous(city) {
 
-var previousURL = "http://api.openweathermap.org/data/2.5/forecast?q=" + city + "&units=metric&appid=75eb6e5eb20db39b88a78417b81c45f6";
+    var previousURL = "http://api.openweathermap.org/data/2.5/forecast?q=" + city + "&units=metric&appid=75eb6e5eb20db39b88a78417b81c45f6";
 
-$.ajax({
-    url: previousURL,
-    method: "GET"
-}).then(function (response) {
+    $.ajax({
+        url: previousURL,
+        method: "GET"
+    }).then(function (response) {
 
-    
 
-    var myArr = []
 
-    for(var i = 0; i < response.list.length; i++ ){
+        var myArr = []
 
-        var arr = response.list[i].dt_txt.split(" ");
+        for (var i = 0; i < response.list.length; i++) {
 
-        if( arr[1] === "12:00:00"){
-            
-            myArr.push(response.list[i]);
+            var arr = response.list[i].dt_txt.split(" ");
 
-            myDate   = myArr[0].dt_txt.split(" ");
-            
+            if (arr[1] === "12:00:00") {
 
-            
-            
-            
-            var icon1 = myArr[0].weather[0].icon;
-        
-            var img1 = '<img src="https://openweathermap.org/img/w/' + icon1 + '.png">';
+                myArr.push(response.list[i]);
 
-            $('#icon1').empty();
-            $('#icon1').append(img1);
-            
-            
-            
-           
-            
-            $("#dateOne").text(myDate[0]);
-            
-            $("#temp1").text("Temp : " + myArr[0].main.temp + "C");
-            $("#humid1").text("Humid : " + myArr[0].main.humidity);
-            $("#des").text(myArr[0].weather[0].description);
+                myDate = myArr[0].dt_txt.split(" ");
 
-            
+
+
+
+
+                var icon1 = myArr[0].weather[0].icon;
+
+                var img1 = '<img src="https://openweathermap.org/img/w/' + icon1 + '.png">';
+
+                $('#icon1').empty();
+                $('#icon1').append(img1);
+
+
+
+
+
+                $("#dateOne").text(myDate[0]);
+
+                $("#temp1").text("Temp : " + myArr[0].main.temp + " C");
+                $("#humid1").text("Humid : " + myArr[0].main.humidity);
+                $("#des").text(myArr[0].weather[0].description);
+
+
+            }
+
         }
 
-    }
+        console.log(myArr[2].main.temp);
 
-    console.log(myArr[2].main.temp);
-    
-    myDate1 = (myArr[1].dt_txt.split(" "));
+        myDate1 = (myArr[1].dt_txt.split(" "));
 
-    $("#dateTwo").text(myDate1[0]);
-    $("#temp2").text("Temp :" + myArr[1].main.temp);
-    $("#humid2").text("Humid :" + myArr[1].main.humidity);
-    $("#des1").text(myArr[1].weather[0].description);
+        $("#dateTwo").text(myDate1[0]);
+        $("#temp2").text("Temp : " + myArr[1].main.temp + " C");
+        $("#humid2").text("Humid : " + myArr[1].main.humidity);
+        $("#des1").text(myArr[1].weather[0].description);
 
-    myDate2 = (myArr[2].dt_txt.split(" "));
-            
-    $("#dateThree").text(myDate2[0]);
-    $("#temp3").text("Temp :" + myArr[2].main.temp);
-    $("#humid3").text("Humid :" + myArr[2].main.humidity);
-    $("#des2").text(myArr[2].weather[0].description);
+        myDate2 = (myArr[2].dt_txt.split(" "));
 
-    myDate3 = (myArr[3].dt_txt.split(" "));
+        $("#dateThree").text(myDate2[0]);
+        $("#temp3").text("Temp : " + myArr[2].main.temp + " C");
+        $("#humid3").text("Humid : " + myArr[2].main.humidity);
+        $("#des2").text(myArr[2].weather[0].description);
 
-    $("#dateFour").text(myDate3[0]);
-    $("#temp4").text("Temp :" + myArr[3].main.temp);
-    $("#humid4").text("Humid :" + myArr[3].main.humidity);
-    $("#des3").text(myArr[3].weather[0].description);
+        myDate3 = (myArr[3].dt_txt.split(" "));
 
-    myDate4 = (myArr[4].dt_txt.split(" "));
+        $("#dateFour").text(myDate3[0]);
+        $("#temp4").text("Temp : " + myArr[3].main.temp + " C");
+        $("#humid4").text("Humid : " + myArr[3].main.humidity);
+        $("#des3").text(myArr[3].weather[0].description);
 
-    $("#dateFive").text(myDate4[0]);
-    $("#temp5").text("Temp :" + myArr[4].main.temp);
-    $("#humid5").text("Humid :" + myArr[4].main.humidity);
-    $("#des4").text(myArr[4].weather[0].description);
+        myDate4 = (myArr[4].dt_txt.split(" "));
+
+        $("#dateFive").text(myDate4[0]);
+        $("#temp5").text("Temp : " + myArr[4].main.temp + " C");
+        $("#humid5").text("Humid : " + myArr[4].main.humidity);
+        $("#des4").text(myArr[4].weather[0].description);
 
 
-    var icon1 = myArr[0].weather[0].icon;
-    var icon2 = myArr[1].weather[0].icon;
-    var icon3 = myArr[2].weather[0].icon;
-    var icon4 = myArr[3].weather[0].icon;
-    var icon5 = myArr[4].weather[0].icon;
+        var icon1 = myArr[0].weather[0].icon;
+        var icon2 = myArr[1].weather[0].icon;
+        var icon3 = myArr[2].weather[0].icon;
+        var icon4 = myArr[3].weather[0].icon;
+        var icon5 = myArr[4].weather[0].icon;
 
 
-        
+
         var img1 = '<img src="https://openweathermap.org/img/w/' + icon1 + '.png">';
         var img2 = '<img src="https://openweathermap.org/img/w/' + icon2 + '.png">';
         var img3 = '<img src="https://openweathermap.org/img/w/' + icon3 + '.png">';
@@ -164,11 +164,11 @@ $.ajax({
         $('#icon5').append(img5);
 
 
-    console.log(myArr[0].weather[0]);
-    
-   
+        console.log(myArr[0].weather[0]);
 
-});
+
+
+    });
 }
 
 // https://api.openweathermap.org/data/2.5/forecast?qlondon&mode=xml&appid=75eb6e5eb20db39b88a78417b81c45f6
